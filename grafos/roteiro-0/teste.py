11689b9102eaf01d9483a6aecdd3338b4c859e1d
Marcus Cauê <marcuscaue.dev@gmail.com>
2024-10-24 11:31:43 -0300

    Roteiro 0 Concluído

--- grafos/roteiro-0/teste.py
+++ grafos/roteiro-0/teste.py
@@ -1,7 +1,7 @@
 from bibgrafo.grafo_lista_adjacencia import GrafoListaAdjacencia, Aresta, Vertice
 
 paraiba = GrafoListaAdjacencia(
-  vertices = ["Jampa", "Monteiro", "Cajazeiras", "Teresina"],
+  vertices = [Vertice("João Pessoa"), Vertice("Monteiro"), Vertice("Cajazeiras"), Vertice("Teresina")],
 )
 
 paraiba.adiciona_vertice("Esperança")
@@ -9,13 +9,13 @@ paraiba.adiciona_vertice("Patos")
 paraiba.adiciona_vertice("Campina")
 
 paraiba.arestas = {
-  "a1": Aresta("a1", "Jampa", "Campina"),
-  "a2": Aresta("a2", "Campina", "Esperança"),
-  "a3": Aresta("a3", "Campina", "Esperança"),
-  "a4": Aresta("a4", "Campina", "Patos"),
-  "a5": Aresta("a5", "Campina", "Patos"),
-  "a6": Aresta("a6", "Campina", "Monteiro"),
-  "a7": Aresta("a7", "Campina", "Teresina"),
+  "a1": Aresta("a1", paraiba.get_vertice("João Pessoa"), paraiba.get_vertice("Campina")),
+  "a2": Aresta("a2", paraiba.get_vertice("Campina"), paraiba.get_vertice("Esperança")),
+  "a3": Aresta("a3", paraiba.get_vertice("Campina"), paraiba.get_vertice("Esperança")),
+  "a4": Aresta("a4", paraiba.get_vertice("Campina"), paraiba.get_vertice("Patos")),
+  "a5": Aresta("a5", paraiba.get_vertice("Campina"), paraiba.get_vertice("Patos")),
+  "a6": Aresta("a6", paraiba.get_vertice("Campina"), paraiba.get_vertice("Monteiro")),
+  "a7": Aresta("a7", paraiba.get_vertice("Campina"), paraiba.get_vertice("Teresina")),
 }
 
 paraiba.adiciona_aresta("a8", "Monteiro", "Teresina")
