06ec1e45ead2dce4b73d666ec7e9d2b2c4a241f0
Marcus Cauê de Farias Barbosa <202321250026@ad.ifpb.edu.br>
2024-12-04 11:34:01 -0300

    Início Roteiro 03

---
+++ grafos/roteiro-03/main.py
@@ -0,0 +1,34 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+from bibgrafo.grafo_builder import GrafoBuilder
+from meu_grafo_lista_adj_nao_dir import MeuGrafo
+from bibgrafo.grafo_lista_adj_nao_dir import Aresta
+from bibgrafo.grafo import Vertice
+
+def plotarGrafo(grafo: MeuGrafo):
+  g = nx.Graph()
+
+  g.add_nodes_from( [ v.rotulo for v in grafo.vertices ] )
+  g.add_edges_from([ (grafo.arestas[a].v1.rotulo, grafo.arestas[a].v2.rotulo) for a in grafo.arestas ])
+
+  nx.draw(g, with_labels=True, arrows=True)
+  plt.show()
+
+# Setup do grafo da paraíba 
+paraiba = GrafoBuilder().tipo(MeuGrafo()) \
+  .vertices(["J", "C", "E", "M", "P", "T", "Z"]) \
+  .arestas([
+    Aresta('a1', Vertice('J'), Vertice('C')),
+    Aresta('a2', Vertice('C'), Vertice('E')),
+    Aresta('a3', Vertice('C'), Vertice('E')),
+    Aresta('a4', Vertice('C'), Vertice('P')),
+    Aresta('a5', Vertice('C'), Vertice('P')),
+    Aresta('a6', Vertice('C'), Vertice('M')),
+    Aresta('a7', Vertice('C'), Vertice('T')),
+    Aresta('a8', Vertice('M'), Vertice('T')),
+    Aresta('a9', Vertice('T'), Vertice('Z')),
+  ]) \
+  .build()
+
+print(plotarGrafo(paraiba))
