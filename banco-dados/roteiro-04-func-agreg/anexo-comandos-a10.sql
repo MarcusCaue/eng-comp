782c419fb787e03748f50a5c3a89bfc8d7869260
Marcus Cauê de Farias Barbosa <202321250026@ad.ifpb.edu.br>
2025-02-13 12:06:20 -0300

    Roteiro 04

--- banco-dados/roteiro-04-func-agreg/anexo-comandos-a10.sql
+++ banco-dados/roteiro-04-func-agreg/anexo-comandos-a10.sql
@@ -1,3 +1,7 @@
+DROP DATABASE IF EXISTS atv_10;
+CREATE DATABASE atv_10;
+USE atv_10;
+
 create table empregado (
  idemp integer PRIMARY KEY, 
  nome varchar(20) NOT NULL, 
@@ -18,7 +22,7 @@ alter table empregado add constraint foreign key (secao) references secao(idsec)
 
 alter table empregado add constraint foreign key (chefe) references empregado (idemp);
 
-insert secao values (10, 'Fabrica', 'Recife'),(20, 'Comercial', 'Recife'), (30, 'Marketing', 'Campina Grande'), (40, 'Planejamento', 'João Pessoa'), (50, 'Administração', 'Recife'), (60, 'Informática', 'Campina Grande'), (70, 'Recursos Humanos', 'João Pessoa')
+insert secao values (10, 'Fabrica', 'Recife'),(20, 'Comercial', 'Recife'), (30, 'Marketing', 'Campina Grande'), (40, 'Planejamento', 'João Pessoa'), (50, 'Administração', 'Recife'), (60, 'Informática', 'Campina Grande'), (70, 'Recursos Humanos', 'João Pessoa');
 
 insert empregado values (3, 'Álvaro Dias', 50, 'Administrador', NULL, 2500, 0);
 insert empregado values (1, 'Ana Paula', 10, 'Programador', 3, 3000, 10); 
@@ -26,7 +30,3 @@ insert empregado values (2, 'Gustavo Henrique', 70, 'Engenheiro', 1, 2500, 50);
 insert empregado values (4, 'Antônio Carlos', 10, 'Engenheiro', 3, 2400, 20);  
 insert empregado values (5, 'Susana Silva', 20, 'Administrador', NULL, 2750, 30);
 insert empregado values (6, 'Cláudio José', 60, 'Vendedor', 4, 1000, 50);  
-
-
-
-
