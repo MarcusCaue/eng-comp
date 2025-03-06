94fbb3996952be1c61b9c39584d2642767ca5965
Marcus Cauê <marcuscaue.dev@gmail.com>
2025-03-05 21:02:30 -0300

    Trabalhar agora nas atualizações

---
+++ banco-dados/projeto-sistema-paroquial/Modelo-Fisico/dml/updates/update.sql
@@ -0,0 +1,7 @@
+-- Nova Inserção após a alimentação inicial
+INSERT INTO Bispo VALUES (4, "Diocese de Palmares", "2000-01-25");
+INSERT INTO Sacerdote VALUES (10, NULL, 4);
+INSERT INTO Pessoa VALUES (25, "Henrique Soares Costa", 64, 10, NULL);
+
+-- Remoção simples
+DELETE From Comunidade
