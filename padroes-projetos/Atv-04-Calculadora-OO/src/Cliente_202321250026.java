2ff0efa399d9639d1ae4a0257d4e0d005770983c
Marcus Cauê <marcuscaue.dev@gmail.com>
2025-04-28 08:27:25 -0300

    Refactoring Cliente

--- padroes-projetos/Atv-04-Calculadora-OO/src/Cliente_202321250026.java
+++ padroes-projetos/Atv-04-Calculadora-OO/src/Cliente_202321250026.java
@@ -1,58 +1,53 @@
+import java.util.ArrayList;
+
 public class Cliente_202321250026 {
+
   public static void main(String args[]) {
     
     Calculadora_202321250026 calc = new Calculadora_202321250026();
-  
-    double n1 = 0;
-    double n2 = 90;
-
-    // System.out.println(90 / 0);
-    System.out.println(n1 / n2);
-
+    
     System.out.println("========== Chamadas à função somar() ==========");
-    System.out.printf("Resultado da soma: %.2f\n", calc.somar(23.45, 10.98));
-    System.out.printf("Resultado da soma: %.2f\n", calc.somar(10.87, -43.56));
-    System.out.printf("Resultado da soma: %.2f\n", calc.somar(-7.44, 86));
-    System.out.printf("Resultado da soma: %.2f\n", calc.somar(-5, -5));
-    System.out.printf("Resultado da soma: %.2f\n", calc.somar(0, 0));
-    System.out.printf("Resultado da soma: %.2f\n", calc.somar(89, 0));
-    System.out.printf("Resultado da soma: %.2f\n", calc.somar(0, 67));
-    System.out.printf("Resultado da soma: %.2f\n\n", calc.somar(0, -76));
+    double[][] sumCaseTests = {
+      {23.45, 10.98}, {10.87, -43.56}, {-7.44, 86}, {-5, -5}, 
+      {0, 0}, {89, 0}, {0, 67}, {0, -76}
+    };
+    
+    for (int i = 0; i < sumCaseTests.length; i++) {
+      System.out.printf("Resultado da soma: %.2f\n", calc.somar(sumCaseTests[i][0], sumCaseTests[i][1]));
+    }
+    
 
-    System.out.println("========== Chamadas à função subtrair() ==========");
-    System.out.printf("Resultado da subtração: %.2f\n", calc.subtrair(5, 7));
-    System.out.printf("Resultado da subtração: %.2f\n", calc.subtrair(215, 5));
-    System.out.printf("Resultado da subtração: %.2f\n", calc.subtrair(7, -2));
-    System.out.printf("Resultado da subtração: %.2f\n", calc.subtrair(-8, 10));
-    System.out.printf("Resultado da subtração: %.2f\n", calc.subtrair(-5, -5));
-    System.out.printf("Resultado da subtração: %.2f\n", calc.subtrair(0, 0));
-    System.out.printf("Resultado da subtração: %.2f\n", calc.subtrair(89, 0));
-    System.out.printf("Resultado da subtração: %.2f\n", calc.subtrair(0, 67));
-    System.out.printf("Resultado da subtração: %.2f\n", calc.subtrair(-90, 0));
-    System.out.printf("Resultado da subtração: %.2f\n\n", calc.subtrair(0, -76));
+    System.out.println("\n========== Chamadas à função subtrair() ==========");
+    double[][] subCaseTests = {
+      {5, 7}, {215, 5}, {7, -2}, {-8, 10}, {-90, 0},
+      {-5, -5}, {0, 0}, {89, 0}, {0, 67}, {0, -76}
+    };
+    
+    for (int i = 0; i < subCaseTests.length; i++) {
+      System.out.printf("Resultado da subtração: %.2f\n", calc.subtrair(subCaseTests[i][0], subCaseTests[i][1]));
+    }
 
-    System.out.println("========== Chamadas à função multiplicar() ==========");
-    System.out.printf("Resultado da multiplicação: %.2f\n", calc.multiplicar(4, 2));
-    System.out.printf("Resultado da multiplicação: %.2f\n", calc.multiplicar(5, -5));
-    System.out.printf("Resultado da multiplicação: %.2f\n", calc.multiplicar(-10, 10));
-    System.out.printf("Resultado da multiplicação: %.2f\n", calc.multiplicar(-4, -6));
-    System.out.printf("Resultado da multiplicação: %.2f\n", calc.multiplicar(0, 0));
-    System.out.printf("Resultado da multiplicação: %.2f\n", calc.multiplicar(89, 0));
-    System.out.printf("Resultado da multiplicação: %.2f\n", calc.multiplicar(0, 67));
-    System.out.printf("Resultado da multiplicação: %.2f\n", calc.multiplicar(-90, 0));
-    System.out.printf("Resultado da multiplicação: %.2f\n\n", calc.multiplicar(0, -76));
 
-    System.out.println("========== Chamadas à função dividir() ==========");
-    System.out.printf("Resultado da divisão: %.2f\n", calc.dividir(10, 5));
-    System.out.printf("Resultado da divisão: %.2f\n", calc.dividir(3, 4));
-    System.out.printf("Resultado da divisão: %.2f\n", calc.dividir(-2, 4));
-    System.out.printf("Resultado da divisão: %.2f\n", calc.dividir(-8, -2));
-    System.out.printf("Resultado da divisão: %.2f\n", calc.dividir(10, -2));
-    System.out.printf("Resultado da divisão: %.2f\n", calc.dividir(0, 67));
-    System.out.printf("Resultado da divisão: %.2f\n", calc.dividir(0, -76));
-    System.out.printf("Resultado da divisão: %.2f\n", calc.dividir(0, 0)); // Divisão por zero => Indefinição
-    System.out.printf("Resultado da divisão: %.2f\n", calc.dividir(89, 0)); // Divisão por zero => Indeterminação
-    System.out.printf("Resultado da divisão: %.2f\n\n", calc.dividir(-90, 0)); // Divisão por zero => Indeterminação
+    System.out.println("\n========== Chamadas à função multiplicar() ==========");
+    double[][] multCaseTests = {
+      {4, 2}, {5, -5}, {-10, 10}, {-4, -6},
+      {0, 0}, {89, 0}, {0, 67}, {-90, 0}, {0, -76}
+    };
 
+    for (int i = 0; i < multCaseTests.length; i++) {
+      System.out.printf("Resultado da multiplicação: %.2f\n", calc.multiplicar(multCaseTests[i][0], multCaseTests[i][1]));
+    }
+
+    System.out.println("\n========== Chamadas à função dividir() ==========");
+    double[][] divCaseTests = {
+      {10, 5}, {3.5, 4}, {-2, 4.1}, {-8, -2},
+      {10.21, -2}, {0, 67}, {0, -76}, {0.42, -76},
+      {0, 0}, {89, 0}, {-90, 0}
+    };
+
+    for (int i = 0; i < divCaseTests.length; i++) {
+      System.out.printf("Resultado da divisão: %.2f\n", calc.dividir(divCaseTests[i][0], divCaseTests[i][1]));
+    }
+    
   }
 }
